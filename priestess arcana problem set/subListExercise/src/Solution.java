03e189e1fbd061021e8a7a5431300791d9df93d6
lordpinpin <113081450+lordpinpin@users.noreply.github.com>
2023-02-08 21:12:38 +0800

    final solution (moved one line) and added comments

--- priestess arcana problem set/subListExercise/src/Solution.java
+++ priestess arcana problem set/subListExercise/src/Solution.java
@@ -17,6 +17,7 @@ import java.util.*;
 
 public class Solution {
 
+    // Swaps position of array
     public static void swap(int[] movie, int i, int j)
     {
         int temp = movie[i];
@@ -24,10 +25,16 @@ public class Solution {
         movie[j] = temp;
     }
 
+    // Finds kth largest number of array. So for example, it can find the 2nd largest number, which would answer the final movie he watched (calculate a/k).
     public static int kthLargest(int[] sublist, int num){
         int startIndex = 0;
         int endIndex = sublist.length - 1;
         int pivot;
+
+        // The loop (I essentially stole the code from sir in Discord).
+
+        // Basically, we do quicksort once then we check if our pivot is the right answer.
+        // If not, we just ignore the side that wouldn't contain our answer and repeat until we hit it.
         while (startIndex < endIndex){
             pivot = startIndex;
             int curSwapIndex = pivot;
@@ -54,6 +61,7 @@ public class Solution {
         return sublist[startIndex];
     }
 
+    // Calculates movie run times since that's all we care about, since he always leaves before the credits.
     public static int[] getMovieRuntimes (int[][] movies){
         int[] movieRuntimes = new int[movies.length];
         int i = 0;
@@ -68,6 +76,7 @@ public class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        // This part just gets input.
         int n = Integer.parseInt(br.readLine().trim());
         int[][] movies = new int[n][2];
         for(int i = 0; i < n; i++) {
@@ -75,8 +84,10 @@ public class Solution {
             movies[i][0] = Integer.parseInt(parts[0]);
             movies[i][1] = Integer.parseInt(parts[1]);
         }
-
         int q = Integer.parseInt(br.readLine().trim());
+
+        // Pre-calculate watch time of each movie (remove credit time from the runtime).
+        int[] movieRunTimes = getMovieRuntimes(movies);
 
         for(int cc = 0; cc < q; cc++) {
             String[] parts = br.readLine().trim().split("\\s+");
@@ -84,11 +95,20 @@ public class Solution {
             int e = Integer.parseInt(parts[1]);
             int a = Integer.parseInt(parts[2]);
             int k = Integer.parseInt(parts[3]);
+            
+            /*
+             a / k will gives us the amount of movies he can watch. Sorting the array every time takes too long,
+             so we just get the nth longest movie, where n is the amount of movies he watches, since he
+             watches from longest to shortest (so his 4th movie will be 4th longest movie.
+            */
+            
             int num = a / k;
-            int[] movieRunTimes = getMovieRuntimes(movies);
             if (num > 0) {
-                int[] sublist = Object.clone()
+                // Gets the sub-array of the specified range (+1 is because the function wouldn't include the last term otherwise)
+                int[] sublist = Arrays.copyOfRange(movieRunTimes, s, e + 1);
                 int ans;
+                
+                // We also check if the money is enough to watch all movies, in which case the movie he last watches is the smallest one.
                 if (e - s + 1 <= num) {
                     ans = kthLargest(sublist, e - s);
                 } else {
